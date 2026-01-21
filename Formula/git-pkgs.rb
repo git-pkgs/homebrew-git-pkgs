c9f7e3a1aa92292ccff52b917627bc9d17d9a071
Andrew Nesbitt <andrewnez@gmail.com>
2026-01-21 10:01:03 +0000

    Fix checksums to match release assets

--- Formula/git-pkgs.rb
+++ Formula/git-pkgs.rb
@@ -7,24 +7,24 @@ class GitPkgs < Formula
   on_macos do
     on_intel do
       url "https://github.com/git-pkgs/git-pkgs/releases/download/v#{version}/git-pkgs-darwin-amd64.tar.gz"
-      sha256 "10383cd286f0038846ceee881f7100b695873c982483a04cd7db7ef02ceaa7c0"
+      sha256 "f750caf7e429974c40428c694f22983de40c10ed90b690bb2698c71006b74850"
     end
 
     on_arm do
       url "https://github.com/git-pkgs/git-pkgs/releases/download/v#{version}/git-pkgs-darwin-arm64.tar.gz"
-      sha256 "e1e16090e39e9d9bb0d2dc9ac39e504f656d1ef1e89e1c437ce3874bc172dea4"
+      sha256 "ee95bd3ee531a0b88313e21fd7063abb457ad9e2e5ba202840301011e1fec032"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/git-pkgs/git-pkgs/releases/download/v#{version}/git-pkgs-linux-amd64.tar.gz"
-      sha256 "f1d8876afaaf40eb54b61371585ee13f161e2710861f8c848d853d3ac6a26443"
+      sha256 "87fb6248a2ffc3c4a96e3d2c1cea258c2de8f68ea6091bf47f1656c034fcf761"
     end
 
     on_arm do
       url "https://github.com/git-pkgs/git-pkgs/releases/download/v#{version}/git-pkgs-linux-arm64.tar.gz"
-      sha256 "d48b89db0d3d939ce4ad75804311b085c110363549d60c4e8d2a694e817419dc"
+      sha256 "10383cd286f0038846ceee881f7100b695873c982483a04cd7db7ef02ceaa7c0"
     end
   end
 
