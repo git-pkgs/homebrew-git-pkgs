9bd944f3904b6fdd1271a4a597298a5ee04d852e
Andrew Nesbitt <andrewnez@gmail.com>
2026-01-26 21:02:51 +0000

    Fix formula for new release asset naming (#2)

--- Formula/git-pkgs.rb
+++ Formula/git-pkgs.rb
@@ -1,35 +1,35 @@
 class GitPkgs < Formula
   desc "Git-native package manager for monorepos"
   homepage "https://github.com/git-pkgs/git-pkgs"
-  version "0.10.4"
+  version "0.10.5"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/git-pkgs/git-pkgs/releases/download/v#{version}/git-pkgs-darwin-amd64.tar.gz"
-      sha256 ""
+      url "https://github.com/git-pkgs/git-pkgs/releases/download/v#{version}/git-pkgs_#{version}_darwin_amd64.tar.gz"
+      sha256 "53c89c83d3a3824a63ec16612f9234d78ace4fe60fa9adc6080aef67d2153829"
     end
 
     on_arm do
-      url "https://github.com/git-pkgs/git-pkgs/releases/download/v#{version}/git-pkgs-darwin-arm64.tar.gz"
-      sha256 ""
+      url "https://github.com/git-pkgs/git-pkgs/releases/download/v#{version}/git-pkgs_#{version}_darwin_arm64.tar.gz"
+      sha256 "ddd6fbb4c65e6cf9579399e63c308c4b8164e517982b311d366917fa6c8100d1"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/git-pkgs/git-pkgs/releases/download/v#{version}/git-pkgs-linux-amd64.tar.gz"
-      sha256 ""
+      url "https://github.com/git-pkgs/git-pkgs/releases/download/v#{version}/git-pkgs_#{version}_linux_amd64.tar.gz"
+      sha256 "aed0559f4e0536f154553d98f180daadf8ed787f4231a6a5cf948a15edfab0f3"
     end
 
     on_arm do
-      url "https://github.com/git-pkgs/git-pkgs/releases/download/v#{version}/git-pkgs-linux-arm64.tar.gz"
-      sha256 ""
+      url "https://github.com/git-pkgs/git-pkgs/releases/download/v#{version}/git-pkgs_#{version}_linux_arm64.tar.gz"
+      sha256 "25224708500231d22b6027e94b1514a6027beace20ad87baa2978e00e5ac3e59"
     end
   end
 
   def install
-    bin.install Dir["git-pkgs-*"].first => "git-pkgs"
+    bin.install "git-pkgs"
     man1.install Dir["man/*.1"]
   end
 
